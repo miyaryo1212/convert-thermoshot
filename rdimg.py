63677a67b8e275e073232dada69e1500c4b95bcc
miyaryo <83214456+miyaryo1212@users.noreply.github.com>
2022-12-02 00:39:08 +0900

    Update rdimg.py
    - convert to HSV
    - save to .xlsx files

--- rdimg.py
+++ rdimg.py
@@ -1,18 +1,39 @@
 import math
+import datetime
 
 import cv2 as cv
 import matplotlib as plt
 import numpy as np
-import openpyxl
+import openpyxl as opxl
 
 
 def rdimg(src):
     img = cv.imread(src)
+    img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    h, s, v = cv.split(img_hsv)
+
     cv.imshow("img", img)
+    cv.imshow("img hsv", img_hsv)
     cv.waitKey(0)
     cv.destroyAllWindows()
+
+    return v
+
+
+def cvtlist(list, path):
+    wb = opxl.Workbook()
+    sheet = wb.worksheets[0]
+
+    for y in range(120):
+        for x in range(160):
+            list_value = list[y][x]
+            sheet.cell(x + 1, y + 1, list_value)
+
+    wb.save(path)
 
 
 if __name__ == "__main__":
     src = "./photos/sample1.jpg"
-    rdimg(src)
+    v = rdimg(src)
+    path = "./output/saved_{}.xlsx".format(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+    cvtlist(v, path)
