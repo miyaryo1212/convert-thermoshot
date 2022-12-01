4373ac7dd1d78f5939070867acfe3c93e96504b6
miyaryo <83214456+miyaryo1212@users.noreply.github.com>
2022-12-02 02:01:04 +0900

    Update rdimg.py
    - ratio by temperature
    - saved message

--- rdimg.py
+++ rdimg.py
@@ -7,13 +7,17 @@ import numpy as np
 import openpyxl as opxl
 
 
-def rdimg(src):
+def rdimg(src, min, max):
     img = cv.imread(src)
     img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
     h, s, v = cv.split(img_hsv)
 
+    for y in range(240):
+        for x in range(320):
+            v[y][x] = 20 + ((max - min) / 255 * v[y][x])
+
     cv.imshow("img", img)
-    #cv.imshow("img hsv", img_hsv)
+    # cv.imshow("img hsv", img_hsv)
     cv.waitKey(0)
     cv.destroyAllWindows()
 
@@ -42,8 +46,13 @@ def cvtlist(list, path):
 
 if __name__ == "__main__":
     src = "./photos/sample1.jpg"
-    v = rdimg(src)
+    v = rdimg(src, 20, 100)
     path = "./output/saved_{}.xlsx".format(
         datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     )
     cvtlist(v, path)
+    print(
+        'Saved in to "saved_{}.xlsx"'.format(
+            datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        )
+    )
