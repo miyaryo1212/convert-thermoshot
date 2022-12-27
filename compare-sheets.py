2ce7ba24140f1c9081ef533b4f2732f120e01bf7
miyaryo <83214456+miyaryo1212@users.noreply.github.com>
2022-12-28 02:36:25 +0900

    Update compare-sheets.py
    - save to .xlsx file
    - change function names

--- compare-sheets.py
+++ compare-sheets.py
@@ -8,6 +8,7 @@ import tkinter
 import tkinter.filedialog
 import tkinter.messagebox
 from tkinter import ttk
+import numpy as np
 
 import openpyxl as opxl
 from openpyxl.utils import get_column_letter
@@ -19,7 +20,7 @@ def showmsgbox(title, content):
     tkinter.messagebox.showinfo(title, content)
 
 
-def choosefile():
+def selectfile():
     root = tkinter.Tk()
     root.withdraw()
     fType = [("", ".xlsx")]
@@ -40,14 +41,14 @@ def detectsheets(booksrc):
     workbook = opxl.load_workbook(booksrc)
     sheets = workbook.sheetnames
 
-    return(sheets)
+    return sheets
 
 
-def choosesheets(list):
+def selectsheets(list):
     root = tkinter.Tk()
     root.withdraw()
 
-    root.title("choose-sheets")
+    root.title("select-sheets")
     root.geometry("300x130")
 
     def ok_get(event):
@@ -60,11 +61,8 @@ def choosesheets(list):
 
     combo_before = ttk.Combobox(root, values=list, justify="center")
     combo_before.grid(column=1, row=0, padx=20, pady=10)
-    combo_before.set(list[0])
     combo_after = ttk.Combobox(root, values=list, justify="center")
     combo_after.grid(column=1, row=1, padx=20, pady=10)
-    combo_after.set(list[1])
-
 
     button = tkinter.Button(root, text="OK")
     button.bind("<Button-1>", ok_get)
@@ -76,7 +74,43 @@ def choosesheets(list):
     root.mainloop()
     root.withdraw()
 
+    if not combo_before.get() or not combo_after.get():
+        print("ERROR: Sheets were not selected correctly")
+        showmsgbox("[Error] select-sheets", "シートが正しく選択されませんでした")
+        quit()
+
     return combo_before.get(), combo_after.get()
+
+
+def readsheet(booksrc, sheet):
+    workbook = opxl.load_workbook(booksrc)
+    sheet = workbook[sheet]
+
+    list = np.zeros((sheet.max_row, sheet.max_column))
+    for row_num in sheet.rows:
+        row = row_num[0].row
+        for column_num in sheet.columns:
+            column = column_num[0].column
+
+            list[row - 1][column - 1] = sheet.cell(
+                row=row, column=column
+            ).value
+
+    return list
+
+
+def writesheet(bookpath, sheetname, list):
+    workbook = opxl.Workbook()
+    sheet = workbook.create_sheet(sheetname)
+
+    for y in range(len(list)):
+        for x in range(len(list[0])):
+            list_value = list[y][x]
+            sheet.cell(y + 1, x + 1, list_value)
+
+    workbook.save(bookpath)
+
+    return None
 
 
 if __name__ == "__main__":
@@ -85,6 +119,25 @@ if __name__ == "__main__":
     except:
         pass
 
-    src = choosefile()
-    sheets = detectsheets(src)
-    print(choosesheets(sheets))
+    src = selectfile()
+    print("INFO: {} loading file...".format(src))
+
+    sheets = selectsheets(detectsheets(src))
+    print("INFO: {}, {} (2 sheets selected)".format(sheets[0], sheets[1]))
+
+    print("INFO: {} converting sheet...".format(sheets[0]))
+    list_before = readsheet(src, sheets[0])
+    print("INFO: {} converted sheet".format(sheets[0]))
+
+    print("INFO: {} converting sheet...".format(sheets[1]))
+    list_after = readsheet(src, sheets[1])
+    print("INFO: {} converted sheet".format(sheets[1]))
+
+    list_delta = list_after - list_before
+
+    time_now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    dir = "./saved"
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    bookpath = "./saved/runned_{}.xlsx".format(time_now)
+    writesheet(bookpath, ("{}-{}".format(sheets[0], sheets[1])), list_delta)
